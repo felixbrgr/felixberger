ac71dab161fcedf3c8ec1a57a303ffa1a4d72e14
Felix Berger <felix.berger@gmail.com>
2020-03-23 19:12:10 +0100

    feature // md content integration.

---
+++ src/markdowns/test.md
@@ -0,0 +1,5 @@
+---
+key: test
+title: test
+---
+text section copy
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,28 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import PageSection from "../components/page-section"
 import TextSection from "../components/text-section"
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
     <PageSection content={{ heading: 'Page section heading' }} >
       <TextSection>
-        <p>Text section copy</p>
+        <div dangerouslySetInnerHTML={{__html: data.markdownRemark.html}}></div>
       </TextSection>
     </PageSection>
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    markdownRemark(fileAbsolutePath: {regex: "/test.md/"}) {
+      html
+    }
+  }
+`
